d0ca7c05b3a878157b3cf68c2475d80d8847262f
Maria <maria.xia@gmail.com>
2016-11-30 01:19:49 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,19 +1,17 @@
 ;(function(){
+
+	// Game module will initiate all parts of the game, and then only handle canvas repainting
 	var Game = function(canvasId){
 		var canvas = document.getElementById(canvasId);
 		var screen = canvas.getContext('2d');
-		this.screen = screen;
-	
-		var screenSize = { x: canvas.width, y: canvas.height };
-		this.screenSize = screenSize;
 
 		var snake = new Snake();
 		var food = new Food();
 	
 		var self = this;
 		var animate = function(){
-				self.update(snake, food);
-				self.draw(screen, snake, food);
+				snake.update(snake, food); // Snake module handles snake behavior
+				self.draw(screen, snake, food); // Game module handles drawing
 			setTimeout(function() {			
 				requestAnimationFrame(animate);}, 70);
 		}
@@ -26,6 +24,7 @@
 	
 		function handler(event){
 			var keyCode = event.code;
+			// handle keypress AND make sure snake isn't able to go directly backwards
 			if (keyCode == 'ArrowLeft' && snake.segments[0].facing != "w")
 				snake.direction = 'e';
 			if (keyCode == 'ArrowRight' && snake.segments[0].facing != "e")
@@ -39,28 +38,27 @@
 	};
 
 	Game.prototype = {
-		// runs the game logic
-		update: function(snake, food){
-			// call update on each of the entities
-			snake.update(snake);
-			// check for collision
-			snake.detectCollision(food);
-		},
-	
 		// draws the game
 		draw: function(screen, snake, food){
-				screen.clearRect(0,0,600,400);
-				screen.fillStyle = 'IndianRed';
-				screen.fillRect(snake.head.x, snake.head.y, 10, 10);
-				snake.segments.forEach(function(segment){
-					screen.fillRect(segment.x, segment.y, 10, 10);
-				});
-				screen.fillStyle = 'OliveDrab';
-				screen.fillRect(food.center.x, food.center.y, 10, 10);
+			// clear for repainting
+			screen.clearRect(0,0,600,400);
+			
+			// draw snake and its segments
+			screen.fillStyle = 'IndianRed';
+			screen.fillRect(snake.head.x, snake.head.y, 10, 10);
+			snake.segments.forEach(function(segment){
+				screen.fillRect(segment.x, segment.y, 10, 10);
+			});
+			
+			// drawing food
+			screen.fillStyle = 'OliveDrab';
+			screen.fillRect(food.center.x, food.center.y, 10, 10);
 		}
 	
 	};
 
+	// Snake module handles all snake behavior:
+	// Moving, eating, collision detection, and resetting
 	var Snake = function(){
 		this.direction = '';
 		this.head = {x: 300, y: 200, facing: ''};
@@ -68,11 +66,14 @@
 	};
 
 	Snake.prototype = {
-
-		update: function(snake){
+		// handles snake behavior (gets called on every animation frame)
+		update: function(snake, food){
 	
+			// with each animation frame, the snake:
+			// first adds its current head to its body...
 			this.segments.unshift({x: this.head.x, y:this.head.y, facing: this.direction});
-		
+			
+			// then pushes out a new head...
 			if (this.direction == "e")
 				this.head.x -= 10;
 			if (this.direction == "w")
@@ -82,14 +83,19 @@
 			if (this.direction == "s")
 				this.head.y += 10;
 			
+			// and then discards from its tail.
 			this.segments.pop();
+			
+			// then checks if it's colliding with something
+			this.detectCollision(food);
 	
 		},
 	
 		eat: function(){
+			// upon eating, snake grows by 3 segments. These segments can be empty because they quickly get swept up by the update function and acquire information.
 			this.segments.push({}, {}, {});
 		
-			// a fun little feature for the title of the game
+			// bonus feature for the title of the game
 			document.getElementById("repeatA").innerHTML += "a";
 		},
 	
@@ -97,18 +103,22 @@
 			this.direction = '';
 			this.head = {x: 300, y: 200};
 			this.segments = [{}];
+			
+			// bonus feature (continued) for title of game
+			document.getElementById("repeatA").innerHTML = "a";
 		},
 		
 		detectCollision: function(food){
+			// detect wall collision
 			if (this.head.x < 0 || this.head.x > 600 || 
 				this.head.y < 0 || this.head.y > 400)
 				this.reset();
-		
+			// detect food collision
 			if (this.head.x == food.center.x && this.head.y == food.center.y){
 				this.eat();
 				food.placeRandom();
 			}
-		
+			// detect collision with self
 			this.segments.forEach(function(segment){
 				if (this.head.x == segment.x && this.head.y == segment.y)
 					this.reset();
@@ -117,6 +127,7 @@
 
 	};
 
+	// Food module handles food placement
 	var Food = function(){
 		this.center = {x: 300, y: 300};
 	};
@@ -129,6 +140,7 @@
 		}
 	};
 
+	// Start the game when everything's loaded!
 	window.addEventListener('load', function() {
 		new Game("screen");
 	});
