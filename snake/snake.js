c92e1f2e25eb11990752710d7d3fb9fba65f76c1
Maria Xia <maria.xia@gmail.com>
2016-12-02 03:01:15 -0500

    Delegated drawing to snake module, removed array method in drawing snake in each animation, should be faster now

--- snake/snake.js
+++ snake/snake.js
@@ -1,58 +1,37 @@
 ;(function(){
 
-	// Game module will initiate all parts of the game, and then only handle canvas repainting
+	// Game module initiates game and animation
 	var Game = function(canvasId){
-		var canvas = document.getElementById(canvasId);
-		var screen = canvas.getContext('2d');
+		var c = document.getElementById(canvasId);
+		var screen = c.getContext('2d');
 
+		// initiate entities
 		var snake = new Snake();
 		var food = new Food();
-	
-		var self = this;
+		food.placeRandom(screen);
+
 		var animate = function(){
-				snake.update(snake, food); // Snake module handles snake behavior
-				self.draw(screen, snake, food); // Game module handles drawing
+			snake.move(screen, snake, food); // Snake module handles snake motion
 			setTimeout(function() {			
-				requestAnimationFrame(animate);}, 70);
+				requestAnimationFrame(animate);
+			}, 60);
 		}
 		animate();
 
-		// handling keypress event
-		// we must use 'keydown' because 'keypress' does not detect arrow presses in chrome
 		// reference: http://unixpapa.com/js/key.html
-		window.addEventListener('keydown', handler);
+		window.addEventListener('keydown', eventHandler);
 	
-		function handler(event){
-			var keyCode = event.code;
+		function eventHandler(ev){
+			var key = ev.code;
 			// handle keypress AND make sure snake isn't able to go directly backwards
-			if (keyCode == 'ArrowLeft' && snake.segments[0].facing != "w")
+			if (key == 'ArrowLeft' && snake.segments[0].facing != "w")
 				snake.direction = 'e';
-			if (keyCode == 'ArrowRight' && snake.segments[0].facing != "e")
+			if (key == 'ArrowRight' && snake.segments[0].facing != "e")
 				snake.direction = 'w';
-			if (keyCode == 'ArrowDown' && snake.segments[0].facing != "n")
+			if (key == 'ArrowDown' && snake.segments[0].facing != "n")
 				snake.direction = 's';
-			if (keyCode == 'ArrowUp' && snake.segments[0].facing != "s")
+			if (key == 'ArrowUp' && snake.segments[0].facing != "s")
 				snake.direction = 'n';
-		}
-	
-	};
-
-	Game.prototype = {
-		// draws the game
-		draw: function(screen, snake, food){
-			// clear for repainting
-			screen.clearRect(0,0,600,400);
-			
-			// draw snake and its segments
-			screen.fillStyle = 'IndianRed';
-			screen.fillRect(snake.head.x, snake.head.y, 10, 10);
-			snake.segments.forEach(function(segment){
-				screen.fillRect(segment.x, segment.y, 10, 10);
-			});
-			
-			// drawing food
-			screen.fillStyle = 'OliveDrab';
-			screen.fillRect(food.center.x, food.center.y, 10, 10);
 		}
 	
 	};
@@ -62,18 +41,20 @@
 	var Snake = function(){
 		this.direction = '';
 		this.head = {x: 300, y: 200, facing: ''};
-		this.segments = [{}];
+		this.segments = [{facing: ''}, {}];
 	};
 
 	Snake.prototype = {
 		// handles snake behavior (gets called on every animation frame)
-		update: function(snake, food){
-	
-			// with each animation frame, the snake:
-			// first adds its current head to its body...
-			this.segments.unshift({x: this.head.x, y:this.head.y, facing: this.direction});
+		move: function(screen, snake, food){
+
+			screen.fillStyle = 'IndianRed';			
 			
-			// then pushes out a new head...
+			// Our snake is endlessly discarding and giving birth to itself. With each animation frame, the snake:
+			// 0. adds its current head to its body...
+			this.segments.unshift({x: this.head.x, y: this.head.y, facing: this.direction});
+			
+			// 1. then pushes out a new head...
 			if (this.direction == "e")
 				this.head.x -= 10;
 			if (this.direction == "w")
@@ -82,46 +63,55 @@
 				this.head.y -= 10;
 			if (this.direction == "s")
 				this.head.y += 10;
+			screen.fillRect(this.head.x, this.head.y, 10, 10);
 			
-			// and then discards from its tail.
+			// 2. and then erases from the tail
+			if (this.direction !== ''){
+				screen.clearRect(this.segments[this.segments.length-1].x, this.segments[this.segments.length -1].y, 10, 10);
+			
+			// 3. then checks if it's colliding with something. 
+			// side note: this next part requires a decision. Should it die as soon as its head meets its tail, even if the tail is right about to get out of the way? (Should we detectCollision before we pop the tail off the end of the array?) This depends on why the snake dies upon meeting itself, and the answer to that is either axiomatic or spiritual.
+			this.detectCollision(screen, food);
+			}
+			
+			// 4. discards tail from the end of the array.
 			this.segments.pop();
-			
-			// then checks if it's colliding with something
-			this.detectCollision(food);
-	
 		},
 	
 		eat: function(){
-			// upon eating, snake grows by 3 segments. These segments can be empty because they quickly get swept up by the update function and acquire information.
+			// upon eating, snake grows by 3 segments. These segments can be empty because they quickly get swept up by the move function and acquire information.
 			this.segments.push({}, {}, {});
 		
 			// bonus feature for the title of the game
 			document.getElementById("repeatA").innerHTML += "a";
 		},
 	
-		reset: function(){
+		reset: function(screen){
+			this.segments.forEach(function(segment){
+				screen.clearRect(segment.x, segment.y, 10, 10);
+			});
 			this.direction = '';
 			this.head = {x: 300, y: 200};
-			this.segments = [{}];
+			this.segments = [{facing: ''}, {}];
 			
 			// bonus feature (continued) for title of game
 			document.getElementById("repeatA").innerHTML = "a";
 		},
 		
-		detectCollision: function(food){
+		detectCollision: function(screen, food){
 			// detect wall collision
 			if (this.head.x < 0 || this.head.x > 600 || 
 				this.head.y < 0 || this.head.y > 400)
-				this.reset();
+				this.reset(screen);
 			// detect food collision
 			if (this.head.x == food.center.x && this.head.y == food.center.y){
 				this.eat();
-				food.placeRandom();
+				food.reset(screen);
 			}
-			// detect collision with self
+			// detect collision with self ... I wonder if this can be optimized.
 			this.segments.forEach(function(segment){
 				if (this.head.x == segment.x && this.head.y == segment.y)
-					this.reset();
+					this.reset(screen);
 			}, this);
 		}
 
@@ -134,9 +124,15 @@
 
 	Food.prototype = {
 		// randomly place yourself somewhere
-		placeRandom: function(){
+		placeRandom: function(screen){
 			this.center.x = Math.floor(Math.random() * 60) * 10;
 			this.center.y = Math.floor(Math.random() * 40) * 10;
+			screen.fillStyle = 'OliveDrab';
+			screen.fillRect(this.center.x, this.center.y, 10, 10);			
+		},
+		
+		reset: function(screen){
+			this.placeRandom(screen);
 		}
 	};
 
